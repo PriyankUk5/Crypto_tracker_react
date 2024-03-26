e8907ca5263cc35a11956a606e6bda75b3cddea7
PriyankUk5 <ukanipriyank5@gmail.com>
2024-03-27 00:04:14 +0530

    code update for crypto hunter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { makeStyles } from "@mui/material";
+import Homepage from "./Pages/HomePage";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
-  return <div>Hello React</div>;
+function App() {
+  const classes = useStyles();
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
